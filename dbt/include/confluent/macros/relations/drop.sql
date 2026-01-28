c6085c8b4601a2f5ba8d89391a3078e936507180
Federico Dolce <fdolce@confluent.io>
2026-01-28 12:03:38 +0100

    reworked drop macros

--- dbt/include/confluent/macros/relations/drop.sql
+++ dbt/include/confluent/macros/relations/drop.sql
@@ -1,26 +1,12 @@
-{% macro confluent__drop_relation(relation) -%}
-  {% if relation.is_table or relation.is_materialized_view -%}
-    {% call statement('drop_relation', fetch_result=False) -%}
-      DROP TABLE IF EXISTS {{ relation }}
-    {% endcall %}
-  {%- elif relation.is_view -%}
-    {% call statement('drop_relation', fetch_result=False) -%}
-      DROP VIEW IF EXISTS {{ relation }}
-    {% endcall %}
-  {%- else -%}
-    {% set msg %}
-      This adapter (confluent) does not support DROP for {{ relation }}.
-    {% endset %}
-    {% do exceptions.raise_compiler_error(msg) %}
-  {%- endif -%}
-{% endmacro %}
-
 {% macro confluent__get_drop_sql(relation) -%}
     {%- if relation.is_view -%}
-        drop view if exists {{ relation.render() }}
-    {%- elif relation.is_table or relation.is_materialized_view -%}
-        drop table if exists {{ relation.render() }}
+        {{ drop_view(relation) }}
+    {%- elif relation.is_table -%}
+        {{ drop_table(relation) }}
+    {%- elif relation.is_materialized_view -%}
+        {{ drop_materialized_view(relation) }}
     {%- else -%}
+        {# We need to customize this to avoid using CASCADE here #}
         drop {{ relation.type }} if exists {{ relation.render() }}
     {%- endif -%}
 {%- endmacro %}
